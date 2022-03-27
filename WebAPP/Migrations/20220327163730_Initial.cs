1b439be83824219c9e73cb64f3b845c5a440d662
flopt04 <zakusenshi@gmail.com>
2022-03-27 17:44:37 +0100

    Products connection with DB

---
+++ WebAPP/Migrations/20220327163730_Initial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPP.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Cod_Produto = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    NomeProduto = table.Column<string>(nullable: true),
+                    Preco = table.Column<int>(nullable: false),
+                    TaxaIVA = table.Column<int>(nullable: false),
+                    Quantidade = table.Column<int>(nullable: false),
+                    Sexo = table.Column<string>(nullable: true),
+                    Tamanho = table.Column<string>(nullable: true),
+                    Tipo = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Cod_Produto);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
