ac58eb80414265ccb61e15ba54807d31bb181124
flopt04 <zakusenshi@gmail.com>
2022-03-27 12:37:38 +0100

    Products View/Controller created

---
+++ WebAPP/Controllers/ShopsCartsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPP.Models;
+
+namespace WebAPP.Controllers
+{
+    public class ShopsCartsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<ShopCart> list = new List<ShopCart>();
+            list.Add(new ShopCart { carrinho_ID = 1 });
+
+            return View();
+        }
+    }
+}
